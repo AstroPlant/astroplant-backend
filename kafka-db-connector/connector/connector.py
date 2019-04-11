f73109fcfe13c01d2699ddc38a1649c0341fa2a9
Thomas Churchman <thomas@kepow.org>
2019-04-11 02:23:12 +0200

    Implement Kafka consumer and insert measurements into the database.

--- kafka-db-connector/connector/connector.py
+++ kafka-db-connector/connector/connector.py
@@ -1,6 +1,64 @@
 import os
 import argparse
-from database import DatabaseManager
+import datetime
+import database as d
+
+
+def utc_from_millis(t):
+    return datetime.datetime.fromtimestamp(t / 1000, datetime.timezone.utc)
+
+
+def _run_connector(db, kafka_consumer):
+    from io import BytesIO
+    import json
+    from sqlalchemy.orm.exc import NoResultFound
+    import fastavro
+
+    with open('./schema/aggregate.avsc', 'r') as f:
+        aggregate_schema = fastavro.parse_schema(json.load(f))
+
+    for record in kafka_consumer:
+        payload = BytesIO(record.value)
+        msg = fastavro.schemaless_reader(payload, aggregate_schema)
+        print(msg)
+
+        try:
+            kit = db.Session\
+                    .query(d.Kit)\
+                    .filter(d.Kit.serial==msg['kit_serial'])\
+                    .one()
+            peripheral = db.Session\
+                           .query(d.Peripheral)\
+                           .filter(
+                               d.Peripheral.name==msg['peripheral'],
+                               d.Peripheral.kit==kit,
+                           )\
+                           .one()
+            qt = db.Session\
+                   .query(d.QuantityType)\
+                   .filter(
+                       d.QuantityType.physical_quantity==msg['physical_quantity'],
+                       d.QuantityType.physical_unit==msg['physical_unit']
+                   )\
+                   .one()
+
+            measurement = d.Measurement(
+                kit=kit,
+                peripheral=peripheral,
+                quantity_type=qt,
+                aggregate_type=msg['type'],
+                value=msg['value'],
+                start_datetime=utc_from_millis(msg['start_datetime']),
+                end_datetime=utc_from_millis(msg['end_datetime']),
+            )
+            db.Session.add(measurement)
+            db.Session.commit()
+            print(kit)
+            print(peripheral)
+        except NoResultFound:
+            # Malformed measurement. Perhaps using an old kit configuration?
+            pass
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -15,7 +73,7 @@ if __name__ == '__main__':
     parser.parse_args()
     args = parser.parse_args()
 
-    db = DatabaseManager(
+    db = d.DatabaseManager(
         host=os.environ.get('DATABASE_HOST', 'database.ops'),
         port=int(os.environ.get('DATABASE_PORT', '5432')),
         username=os.environ.get('DATABASE_USERNAME', 'astroplant'),
@@ -27,5 +85,15 @@ if __name__ == '__main__':
     elif args.command == 'insert-develop-data':
         db.insert_develop_data()
     elif args.command == 'run':
-        raise NotImplementedError()
+        from kafka import KafkaConsumer
 
+        kafka_host = os.environ.get('KAFKA_HOST', 'kafka.ops')
+        kafka_port = int(os.environ.get('KAFKA_PORT', '9092'))
+
+        kafka_consumer = KafkaConsumer(
+            'measurement_aggregate',
+            #group_id='db-connector',
+            bootstrap_servers=[f'{kafka_host}:{kafka_port}'],
+        )
+
+        _run_connector(db, kafka_consumer)
