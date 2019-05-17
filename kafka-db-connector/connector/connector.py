138f570e0d09152b3d8dd01089409a63989e97d4
David Salek <salekd@gmail.com>
2019-05-17 14:24:25 +0200

    consume earliest

--- kafka-db-connector/connector/connector.py
+++ kafka-db-connector/connector/connector.py
@@ -151,6 +151,7 @@ if __name__ == '__main__':
             'aggregate-schema',
             bootstrap_servers=[f'{kafka_host}:{kafka_port}'],
             group_id=kafka_consumer_group,
+            auto_offset_reset='earliest',  # Consume earliest available message.
             security_protocol="SASL_PLAINTEXT",
             sasl_mechanism="PLAIN",
             sasl_plain_username=kafka_username,
