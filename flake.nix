6237606e5244c6c2356316084c95a0e496e55160
Thomas Churchman <thomas@kepow.org>
2022-06-28 00:15:59 +0200

    Flake: proxy astroplant-api websocket in container

--- flake.nix
+++ flake.nix
@@ -70,6 +70,7 @@
                 default = true;
                 locations."/" = {
                   proxyPass = "http://127.0.0.1:8080";
+                  proxyWebsockets = true;
                 };
               };
             };
