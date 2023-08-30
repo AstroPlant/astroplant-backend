d4c6c526938b332fc3675856a398d2986e5581fc
Thomas Churchman <thomas@kepow.org>
2023-08-30 11:56:14 +0200

    Use astroplant-admin binary in docker compose db setup

--- database/docker/init.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export DATABASE_URL=postgresql+psycopg2://astroplant@/astroplant
-
-astroplant-database create-schema
-astroplant-database insert-definitions --simulation-definitions
